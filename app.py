520cf3490902f615bf9f6911175a723c2e4072be
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-02-05 13:18:56 -0500

    Add docker stuff

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/description', methods=['POST'])
+def description():
+    request_data = request.get_json()
+    description = request_data['description']
+    return process_description(description)
+    
+def process_description(description):
+    # Your code here
+    pass
+
+if __name__ == '__main__':
+    app.run(port=5000,host="0.0.0.0")
