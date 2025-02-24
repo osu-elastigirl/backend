500fa7abd4590eb9da0d847d26db6a2d9dff52c8
Kiran Wijesooriya <kiranw1000@gmail.com>
2025-02-24 10:51:31 -0500

    Allow cors once dockerized

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
 from ai import get_recommendations
-import json
+import json, flask_cors
+
+flask_cors.logging.getLogger('flask_cors').level = flask_cors.logging.DEBUG
 
 app = Flask(__name__)
+cors = flask_cors.CORS(app, resources={r"/api/*": {"origins": "*", "allow_headers": "*", "expose_headers": "*"}})
 gemini_key = json.load(open('config.json'))['gemini_key']
 
 @app.route('/')
 def hello_world():
+    print('Hello, World!')
     return 'Hello, World!'
 
-@app.route('/description', methods=['GET'])
+@app.route('/description', methods=['POST'])
+@flask_cors.cross_origin()
 def description():
-    print(request)
+    print(request.get_json())
     request_data = request.get_json()
     description = request_data['description']
     return process_description(description)
@@ -20,4 +25,4 @@ def process_description(description):
     return get_recommendations(description, gemini_key).text
 
 if __name__ == '__main__':
-    app.run(port=5000,host="0.0.0.0")
+    app.run(port=5000, debug=True, host='0.0.0.0')
